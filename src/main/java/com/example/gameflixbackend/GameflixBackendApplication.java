ccc4753a279163470059f71df3af514addceb2ef
David <david.snaxx@gmail.com>
2025-11-09 17:03:09 -0500

    Initial commit

---
+++ src/main/java/com/example/gameflixbackend/GameflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameflixBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gameflixbackend/GameflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameflixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
