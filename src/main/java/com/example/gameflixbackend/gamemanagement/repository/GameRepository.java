dee0c487dca9c42604e8978bff9b677b7dcef46c
David <david.snaxx@gmail.com>
2025-11-09 15:55:49 -0500

    Added basic save, get, and delete from database methods to GameServiceImpl in gamemanagement

--- src/main/java/com/example/gameflixbackend/gamemanagement/repository/GameRepository.java
+++ src/main/java/com/example/gameflixbackend/gamemanagement/repository/GameRepository.java
@@ -1,4 +1,13 @@
 package com.example.gameflixbackend.gamemanagement.repository;
 
-public interface GameRepository {
+import com.example.gameflixbackend.gamemanagement.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game,Long> {
 }
--- src/main/java/com/example/gameflixbackend/gamemanagement/service/GameServiceImpl.java
+++ src/main/java/com/example/gameflixbackend/gamemanagement/service/GameServiceImpl.java
@@ -1,4 +1,34 @@
 package com.example.gameflixbackend.gamemanagement.service;
 
-public class GameServiceImpl {
+import com.example.gameflixbackend.gamemanagement.model.Game;
+import com.example.gameflixbackend.gamemanagement.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class GameServiceImpl implements GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Override
+    public void save(Game game) {
+        this.gameRepository.save(game);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.gameRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Game> findById(Long id) {
+        Optional<Game> game = this.gameRepository.findById(id);
+        if (game.isPresent()) {
+            return game;
+        }
+        throw new RuntimeException("Game with id " + id + " not found");
+    }
 }
