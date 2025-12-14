5be9856848d316f41455e0022826a1192fad29df
David <david.snaxx@gmail.com>
2025-12-13 23:43:02 -0500

    admin cleanup

--- src/main/java/com/example/gameflixbackend/pagerender/controller/AdminPageController.java
+++ src/main/java/com/example/gameflixbackend/pagerender/controller/AdminPageController.java
@@ -4,25 +4,29 @@ import com.example.gameflixbackend.gamemanagement.model.Game;
 import com.example.gameflixbackend.gamemanagement.model.IgdbSearchResult;
 import com.example.gameflixbackend.gamemanagement.repository.GameRepository;
 import com.example.gameflixbackend.gamemanagement.service.IgdbService;
+import com.example.gameflixbackend.usermanagement.model.User;
+import com.example.gameflixbackend.usermanagement.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 public class AdminPageController {
 
     private IgdbService igdbService;
     private GameRepository gameRepository;
+    private UserRepository userRepository;
 
-    public AdminPageController(IgdbService igdbService, GameRepository gameRepository) {
+    public AdminPageController(IgdbService igdbService, GameRepository gameRepository, UserRepository userRepository) {
         this.igdbService = igdbService;
         this.gameRepository = gameRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/admin")
@@ -68,5 +72,60 @@ public class AdminPageController {
             this.gameRepository.delete(toDelete);
         }
         return "redirect:/admin/all-games";
+    }
+
+    @GetMapping("admin/all-users")
+    public String renderAllUsersPage(Model model) {
+        List<User> users = this.userRepository.findAll();
+        model.addAttribute(users);
+        return "admin-users-list";
+    }
+
+    @PostMapping("admin/all-users/delete")
+    public String deleteUser(@RequestParam("userId") Long userId) {
+        Optional<User> optionalUser = this.userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            return "redirect:/admin/all-users?success=false";
+        }
+
+        User user = optionalUser.get();
+        this.userRepository.delete(user);
+        return "redirect:/admin/all-users?success=true";
+    }
+
+    @PostMapping("admin/all-users/toggleSub")
+    public String toggleSubscriptionState(@RequestParam("userId") Long userId) {
+        Optional<User> optionalUser = this.userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            return "redirect:/admin/all-users?success=false";
+        }
+
+        User user = optionalUser.get();
+        // flip-flop the subscriber boolean
+        if (user.isSubscribed) {
+            user.setSubscribed(false);
+        } else {
+            user.setSubscribed(true);
+        }
+        this.userRepository.save(user);
+        return "redirect:/admin/all-users?success=true";
+    }
+
+    @PostMapping("admin/all-users/toggleAdmin")
+    public String toggleAdminState(@RequestParam("userId") Long userId) {
+        Optional<User> optionalUser = this.userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            return "redirect:/admin/all-users?success=true";
+        }
+
+        User user = optionalUser.get();
+        // flip-flop the admin boolean
+        if (user.isAdmin) {
+            user.setAdmin(false);
+        } else {
+            user.setAdmin(true);
+        }
+        this.userRepository.save(user);
+        return "redirect:/admin/all-users?success=true";
     }
 }
